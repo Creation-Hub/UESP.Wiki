f42a6531aa65f6293389313e6c7f75e293672356
Scrivener07 <ScrivenerMods@gmail.com>
2025-05-31 07:19:25 -0400

    Update meta.py

--- app/wiki/meta.py
+++ app/wiki/meta.py
@@ -8,14 +8,14 @@ DATE_TIME_FORMAT = "%Y/%m/%d %I:%M:%S %p"
 """Format for date and time string as YYYY/MM/DD hh:mm:ss AM/PM."""
 
 
-def generation_date():
+def generation_date() -> str:
     """Gets the current date and time."""
     dt = datetime.datetime.now()
     pretty_date = dt.strftime(DATE_TIME_FORMAT)
     return f"* GENERATED: {pretty_date}"
 
 
-def script_date_modified(script_path):
+def script_date_modified(script_path:str) -> str:
     """Gets the last modified date of the source script file."""
     mod_time = os.path.getmtime(script_path)
     dt = datetime.datetime.fromtimestamp(mod_time)
@@ -25,7 +25,7 @@ def script_date_modified(script_path):
 # Writer
 #---------------------------------------------
 
-def write(file, script_path):
+def write(file, script_path:str):
     """Writes meta data related to the generator and game files."""
     file.write(generation_date())
     file.write("\n")
