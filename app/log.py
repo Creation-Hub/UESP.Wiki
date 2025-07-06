335b9217ef4a6b042b9fefd2a5c9af8539cb4d5e
Scrivener07 <ScrivenerMods@gmail.com>
2025-07-06 19:50:47 -0400

    Improve logging format and add date formatting
    - Introduced a consistent date format for log messages and enhanced the file handler log format to include module and function names.
    - Separate formatters are now used for file and console handlers.

--- app/log.py
+++ app/log.py
@@ -1,19 +1,22 @@
 import logging
 
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
 def configure():
     # Create root logger
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     # Log File handler
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s', datefmt=DATE_FORMAT)
     file_handler = logging.FileHandler("app.log", mode='w')
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(file_formatter)
     file_handler.setLevel(logging.DEBUG)
     logger.addHandler(file_handler)
 
     # Console Stream handler
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt=DATE_FORMAT)
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
+    console_handler.setFormatter(console_formatter)
     console_handler.setLevel(logging.INFO)
     logger.addHandler(console_handler)
